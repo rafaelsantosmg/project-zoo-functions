855a9a2fa22892efedec4190c35558fa0e14524f
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2021-10-13 15:06:24 -0300

    requisito 4 getRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
   // seu código aqui
+  return employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return employees.reduce((acc, current) => {
+    if (current.managers.includes(managerId)) {
+      acc.push(`${current.firstName} ${current.lastName}`);
+    }
+    return acc;
+  }, []);
 }
 
 module.exports = { isManager, getRelatedEmployees };
