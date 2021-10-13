4299131547bd582b023bffbb916d5d5172d5156b
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2021-10-13 15:25:14 -0300

    requisito 5 countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const countResidents = (resident) => {
+  const residents = species.reduce((acc, specie) =>
+    (specie.name === resident.specie ? specie.residents : acc));
+  if (!resident.sex) return residents.length;
+  return residents.reduce((acc, currentSpecie) => (
+    currentSpecie.sex === resident.sex ? acc + 1 : acc), 0);
+};
+
 function countAnimals(animal) {
   // seu código aqui
+  if (animal === undefined) {
+    return data.species.reduce((acc, specie) => {
+      acc[specie.name] = specie.residents.length;
+      return acc;
+    }, {});
+  }
+  return countResidents(animal);
 }
 
 module.exports = countAnimals;
