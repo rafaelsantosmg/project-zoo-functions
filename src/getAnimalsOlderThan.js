c7ba04a9a9e32a1a1f6aeab65af38a311a1b151a
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2021-10-13 15:02:32 -0300

    requisito 2 getAnimalOtherThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  return species.some((specie) => animal === specie.name
+  && specie.residents.every((ages) => ages.age >= age));
 }
 
 module.exports = getAnimalsOlderThan;
