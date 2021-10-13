0419de1c9db1dd423bf765f2b1a01fa7cd67ca62
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2021-10-13 19:02:03 -0300

    requisito 6 calculateEntry

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,38 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
+const entrantsteste = [
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+];
+
 function countEntrants(entrants) {
   // seu código aqui
+  let child = 0;
+  let adult = 0;
+  let senior = 0;
+  entrants.forEach((element) => {
+    if (element.age < 18) child += 1;
+    if (element.age >= 18 && element.age < 50) adult += 1;
+    if (element.age >= 50) senior += 1;
+  });
+  return { child, adult, senior };
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  if (!entrants || Object.values(entrants).length === 0) return 0;
+  const entrant = countEntrants(entrants);
+  let total = 0;
+  total += entrant.child * prices.child;
+  total += entrant.adult * prices.adult;
+  total += entrant.senior * prices.senior;
+  return total;
 }
 
 module.exports = { calculateEntry, countEntrants };
--- src/countAnimals.js
+++ src/countAnimals.js
@@ -5,6 +5,7 @@ const { species } = data;
 const countResidents = (resident) => {
   const residents = species.reduce((acc, specie) =>
     (specie.name === resident.specie ? specie.residents : acc));
+  console.log(residents);
   if (!resident.sex) return residents.length;
   return residents.reduce((acc, currentSpecie) => (
     currentSpecie.sex === resident.sex ? acc + 1 : acc), 0);
@@ -13,12 +14,14 @@ const countResidents = (resident) => {
 function countAnimals(animal) {
   // seu código aqui
   if (animal === undefined) {
-    return data.species.reduce((acc, specie) => {
+    return species.species.reduce((acc, specie) => {
       acc[specie.name] = specie.residents.length;
       return acc;
     }, {});
   }
   return countResidents(animal);
 }
+
+countAnimals({ specie: 'bears', sex: 'male' });
 
 module.exports = countAnimals;
