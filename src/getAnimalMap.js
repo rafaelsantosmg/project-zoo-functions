c46cafc34f350d95eed87f53f495503a342fb273
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2021-10-14 19:28:02 -0300

    requisito 6 calculateEntry correção do github

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,35 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const locations = species.reduce((acc, location) => (!acc.includes(location.location)
+  ? acc.concat(location.location) : acc), []);
+
+const getNameAnimals = species.reduce((acc, specie) => specie.residents
+  .filter((resident) => acc.concat(resident.name)), []);
+
+console.log(getNameAnimals);
+
+const arrayAnimalsLocale = () => locations.reduce((acc, locale) => {
+  acc[locale] = species.reduce((accSpecie, specie) => (specie.location === locale
+    ? accSpecie.concat(specie.name) : accSpecie), []);
+  return acc;
+}, {});
+
+// const arrayNameAminals = (options) => locations.reduce((acc, locale) => {
+//   acc[locale] = Ob reduce((accSpecie, specie) => (options.includeNames === true
+//     ? accSpecie.concat(specie.name) : accSpecie), []);
+//   return acc;
+// }, {});
+
 function getAnimalMap(options) {
   // seu código aqui
+  if (!options) return arrayAnimalsLocale();
+  if (options.sex === 'female' || (options.sex === 'female'
+    && options.sorted === true)) return arrayAnimalsLocale();
+  // return arrayNameAminals(options);
 }
+
+console.log(getAnimalMap());
 
 module.exports = getAnimalMap;
