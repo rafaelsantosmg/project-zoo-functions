5b4f4994007d77ccf2a6860536e318e1e6f8c6b7
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2021-10-15 16:19:51 -0300

    requisito 9 getOldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
-const data = require('../data/zoo_data');
+const { species, employees } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const employeesRessibleId = employees.find((employee) => employee.id
+  === id).responsibleFor;
+  return species.reduce((acc, specie) => (employeesRessibleId[0] === specie.id
+    ? Object.values(specie.residents.reduce((accResident, resident) => (
+      accResident.age > resident.age
+        ? accResident : resident))) : acc), []);
 }
 
 module.exports = getOldestFromFirstSpecies;
